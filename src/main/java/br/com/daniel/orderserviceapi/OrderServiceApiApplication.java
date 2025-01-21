fa76e37322ce82eaf6d593cad90664d4252f3a73
Daniel Paulino <daniel-paulino@hotmail.com>
2025-01-21 15:24:31 -0300

    feat: created entity Order and saved order by CommandLine

--- src/main/java/br/com/daniel/orderserviceapi/OrderServiceApiApplication.java
+++ src/main/java/br/com/daniel/orderserviceapi/OrderServiceApiApplication.java
@@ -1,10 +1,23 @@
 package br.com.daniel.orderserviceapi;
 
+import br.com.daniel.orderserviceapi.entities.Order;
+import br.com.daniel.orderserviceapi.repositories.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApiApplication {
+@RequiredArgsConstructor
+public class OrderServiceApiApplication implements CommandLineRunner {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // run belongs to CommandLineRunner, is one way to execute instructions when application is starting
+        orderRepository.save(new Order(null, "1","2","Order 1","Description of order", null, null));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApiApplication.class, args);
---
+++ src/main/java/br/com/daniel/orderserviceapi/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package br.com.daniel.orderserviceapi.repositories;
+
+import br.com.daniel.orderserviceapi.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
