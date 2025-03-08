bbd55bed09dfc719f4e80cbce4e91d2d653b2aac
Daniel Paulino <daniel-paulino@hotmail.com>
2025-03-08 18:47:38 -0300

    feat: added producer of RabbitMq for creating order.

--- src/main/java/br/com/daniel/orderserviceapi/service/impl/OrderServiceImpl.java
+++ src/main/java/br/com/daniel/orderserviceapi/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import br.com.daniel.orderserviceapi.entities.Order;
 import br.com.daniel.orderserviceapi.mapper.OrderMapper;
 import br.com.daniel.orderserviceapi.repositories.OrderRepository;
 import br.com.daniel.orderserviceapi.service.OrderService;
+import br.com.userservice.commonslib.model.dtos.OrderCreatedMessage;
 import br.com.userservice.commonslib.model.enums.OrderStatusEnum;
 import br.com.userservice.commonslib.model.exceptions.ResourceNotFoundException;
 import br.com.userservice.commonslib.model.requests.CreateOrderRequest;
@@ -13,6 +14,7 @@ import br.com.userservice.commonslib.model.responses.OrderResponse;
 import br.com.userservice.commonslib.model.responses.UserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -29,6 +31,7 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
     private final UserServiceFeignClient userServiceFeignClient;
+    private final RabbitTemplate rabbitTemplate;
 
     @Override
     public Order findById(final Long id) {
@@ -49,6 +52,15 @@ public class OrderServiceImpl implements OrderService {
 
         final var entity = orderRepository.save(orderMapper.fromRequest(createOrderRequest));
         log.info("Order saved: {}", entity);
+
+        rabbitTemplate.convertAndSend(
+                "newhelpdesk",
+                "rk.orders.create",
+                new OrderCreatedMessage(
+                        orderMapper.fromEntity(entity),
+                        customer,
+                        requester
+                ));
     }
 
     @Override
